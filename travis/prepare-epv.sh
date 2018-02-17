7255b2450bafdbf4479c4a22d0b36f4bc3b3e35e
Jasolo <36385249+jasoloz@users.noreply.github.com>
2018-02-17 21:06:18 +0100

    More travis fixes

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -12,12 +12,11 @@ set -e
 set -x
 
 EPV=$1
-NOTESTS=$2
+TRAVIS_PHP_VERSION=$2
 
-if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
+if [ "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.5" ]
 then
 	cd phpBB
-	composer remove sami/sami --update-with-dependencies --dev --no-interaction
-	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
+	composer require phpbb/epv:dev-master --dev --no-interaction
 	cd ../
 fi
