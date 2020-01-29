6d0f9268629b43ba9adfe9a247a4f25cc6c30b7f
Jasolo <36385249+jasoloz@users.noreply.github.com>
2020-01-29 19:06:13 +0100

    Add files via upload

--- language/de/advancedpolls.php
+++ language/de/advancedpolls.php
@@ -44,13 +44,49 @@ $lang = array_merge($lang, array(
 
 	'AP_POLL_CANT_VOTE'						=> 'Du kannst bei dieser Umfrage nicht abstimmen. Grund',
 	'AP_POLL_REASON_NOT_POSTED'				=> 'Du hast noch keinen Beitrag in diesem Thema geschrieben.',
-	'AP_POLL_VOTES_ARE_VISIBLE'				=> 'Bitte beachte, dass wenn du abstimmst, wird deine Stimme sichtbar sein.',
+	'AP_POLL_VOTES_ARE_VISIBLE'				=> 'Bitte beachte, falls Du abstimmst, wird Deine Stimme sichtbar sein.',
 	'AP_POLL_DONT_VOTE_SHOW_RESULTS'		=> 'Nicht abstimmen, Ergebnis anzeigen',
+	'AP_POLL_RESULTS_ARE_ORDERED'			=> 'Bitte beachte, dass die Ergebnisse nach Anzahl der abgegebenen Stimmen absteigend sortiert sind.',
+	'AP_POLL_TYPE_MISMATCH'					=> 'Interner Fehler. Inkonsistente Abstimmungsdaten.',
+	'AP_VOTE_CHANGED'						=> 'Du hast nicht die Berechtigung, Deine abgegbenen Stimmen zu ändern.',
+	'AP_TOO_MANY_VOTES'						=> 'Du hast versucht, zuviele Stimmen abzugeben.',
 
-	'AP_POLL_VOTES_HIDE'					=> 'Verberge Abstimmungen',
-	'AP_POLL_VOTES_HIDE_EXPLAIN'			=> 'Wenn diese Option aktiviert ist, werden Abstimmungen verborgen, bis die Umfrage beendet ist.<br />Diese Option funktioniert nur, wenn ein End-Datum für diese Umfrage setzt ist.',
-	'AP_POLL_VOTERS_SHOW'					=> 'Zeige Benutzer, die abgestimmt haben',
-	'AP_POLL_VOTERS_SHOW_EXPLAIN'			=> 'Wenn diese Option aktiviert ist, werden Benutzer, die abgestimmt haben, für alle mit den entsprechenden Berechtigungen unter den Optionen angezeigt.<br />Beachte, dass dies verborgen bleibt, falls die Abstimmungen verborgen sein sollten.',
-	'AP_POLL_VOTERS_LIMIT'					=> 'Schränke Abstimmung ein',
-	'AP_POLL_VOTERS_LIMIT_EXPLAIN'			=> 'Wenn diese Option aktiviert ist, werden Benutzer nur abstimmen können, wenn sie bereits in diesem Thema geschrieben haben.',
+	'AP_MAX_VOTES_SELECT'					=> array(
+		1	=> 'Du kannst bis zu <strong>%2$d</strong> Stimmen <strong>%1$d</strong>er Wahlmöglichkeit geben',
+		2	=> 'Du kannst bis zu <strong>%2$d</strong> Stimmen auf <strong>%1$d</strong> Wahlmöglichkeiten verteilen',
+	),
+	'AP_GUEST_VOTES'						=> array(
+		1	=> '%d Stimme von Gästen',
+		2	=> '%d Stimmen von Gästen',
+	),
+
+// Posting
+	'AP_POLL_VOTES_HIDE'					=> 'Abstimmungsergebnisse verbergen',
+	'AP_POLL_VOTES_HIDE_EXPLAIN'			=> 'Wenn diese Option aktiviert ist, werden die Abstimmungsergebnisse verborgen, bis die Umfrage beendet ist.<br />Diese Option funktioniert nur, wenn ein Endedatum für diese Umfrage gesetzt ist.',
+	'AP_POLL_VOTERS_SHOW'					=> 'Benutzer, die abgestimmt haben, anzeigen',
+	'AP_POLL_VOTERS_SHOW_EXPLAIN'			=> 'Wenn diese Option aktiviert ist, werden Benutzer, die abgestimmt haben, für alle mit entsprechenden Berechtigungen sichtbar sein.<br />Beachte, dass diese Benutzer verborgen bleibt, falls die Abstimmungen geheim sein sollten.',
+	'AP_POLL_VOTERS_LIMIT'					=> 'Umfrage einschränken',
+	'AP_POLL_VOTERS_LIMIT_EXPLAIN'			=> 'Wenn diese Option aktiviert ist, werden Benutzer nur abstimmen können, wenn sie in diesem Thema bereits etwas geschrieben haben.',
+	'AP_POLL_SHOW_ORDERED'					=> 'Ergebnisse sortiert anzeigen',
+	'AP_POLL_SHOW_ORDERED_EXPLAIN'			=> 'Wenn die Ergebnisse angezeigt werden, werden sie absteigend nach der Anzahl der erhaltenen Stimmen angezeigt (am meisten Gewähltes zuerst). Ansonsten wird die Sortierung gemäss Umfrageoption verwendet.',
+	'AP_RUN_POLL'							=> 'Umfrage starten',
+	'AP_RUN_POLL_FOR'						=> 'für',
+	'AP_RUN_POLL_UNTIL'						=> 'bis',
+	'AP_RUN_POLL_INDEFINITELY'				=> 'unendlich',
+	'AP_POLL_END'							=> 'Umfrageende',
+	'AP_POLL_END_EXPLAIN'					=> 'Gib das Datum und die Uhrzeit an, zu denen die Umfrage beendet sein soll. Falls eines dieser Felder ausgefüllt ist, überschreibt es die Umfragedauer. Bei leerem Datumsfeld wird das aktuelle Umfrage-Endedatum verwendet; ein leeres Zeitfeld bedeutet 00:00 Uhr. Möchtest Du wieder die Umfragedauer nutzen, musst Du diese Felder leeren.',
+
+	'AP_YYYY_MM_DD'							=> 'YYYY-MM-DD',
+	'AP_HH_MM'								=> 'HH:MM',
+	'AP_POLL_END_INVALID'					=> 'Ungültiges Datum/ ungültige Uhrzeit angegeben',
+	'AP_POLL_TOTAL_LOWER_MAX_VOTES'			=> 'Die maximale Anzahl Stimmen für eine Auswahlmöglichkeit kann nicht grösser sein als die Gesamtzahl aller Stimmen für alle Wahlmöglichkeiten',
+	'AP_POLL_TOTAL_LOWER_MAX_OPTS'			=> 'Die maximale Anzahl aller Auswahlmöglichkeiten kann nicht größer sein als die maximale Anzahl Stimmen, die auf diese Wahlmöglichkeiten verteilt werden können',
+
+	'AP_POLL_MAX_VALUE'						=> 'Maximale Anzahl Stimmen pro Auswahlmöglichkeit',
+	'AP_POLL_MAX_VALUE_EXPLAIN'				=> 'Dies ist die maximale Anzahl Stimmen, die ein Benutzer einer Auswahlmöglichkeit geben kann.',
+	'AP_POLL_TOTAL_VALUE'					=> 'Gesamtanzahl Stimmen',
+	'AP_POLL_TOTAL_VALUE_EXPLAIN'			=> 'Dies ist die gesamte Anzahl von Stimmen, die ein Benutzer auf alle Auswahlmöglichkeiten verteilen kann.',
+
+	'AP_VOTE_GREATER_THAN_MAXVALUE'			=> 'Du kannst nicht mehr Stimmen vergeben, als maximal erlaubt sind.',
 ));
+
